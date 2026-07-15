a01f153b5773f98a69cf5e2ca1ef832d35cc1cc2
Loveleet Verma <58814550+voltt-cell@users.noreply.github.com>
2026-07-15 06:28:22 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (53.22%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,17 +4,29 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   const x=new Map();
+//    const x=new Map();
 
-   nums.forEach((n,i)=>x.set(n,i))
+//    nums.forEach((n,i)=>x.set(n,i))
 
-   for(let i=0;i<nums.length;i++)
-   {
+//    for(let i=0;i<nums.length;i++)
+//    {
+//     const a=target-nums[i];
+//     if(x.has(a) && x.get(a)!==i)
+//     {
+//         return [i,x.get(a)]
+//     }
+//    }
+
+const x={}
+
+for(let i=0;i<nums.length;i++)
+{
     const a=target-nums[i];
-    if(x.has(a) && x.get(a)!==i)
+    if(a in x)
     {
-        return [i,x.get(a)]
+        return [i,x[a]]
     }
-   }
+    x[nums[i]]=i
+}
 
 };

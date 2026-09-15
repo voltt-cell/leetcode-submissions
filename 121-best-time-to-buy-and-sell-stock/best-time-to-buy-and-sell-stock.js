d6ae35eb425695344924e155c35a5dda094faf69
Loveleet Verma <58814550+voltt-cell@users.noreply.github.com>
2026-09-15 12:40:22 +0530

    Time: 1 ms (94.95%) | Memory: 64.9 MB (73.35%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -13,14 +13,28 @@ var maxProfit = function(prices) {
     // }
     // return max
 
-    let min=prices[0]; let max=0
+    // let min=prices[0]; let max=0
+    // for(let i=0;i<prices.length;i++)
+    // {
+    //     if(prices[i]<min)
+    //     {
+    //         min=prices[i]
+    //     }
+    //     else if( (prices[i]- min) > max)
+    //     {
+    //         max=prices[i]-min
+    //     }
+    // }
+    // return max
+
+
+
+    let min=prices[0], max=0
     for(let i=0;i<prices.length;i++)
     {
         if(prices[i]<min)
-        {
-            min=prices[i]
-        }
-        else if( (prices[i]- min) > max)
+        min=prices[i]
+        else if(prices[i]-min>max)
         {
             max=prices[i]-min
         }
